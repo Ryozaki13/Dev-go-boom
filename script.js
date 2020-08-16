64b2231f119d18a4092eb5a0972098a91417677a
Kieron Webber <kieron_webber@hotmail.co.uk>
2020-08-16 21:35:16 +0100

    added Vaiables to cript.js to create a list using js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 console.log('works')
 var titleHeader = 'Bolton Builder';
-var list = 'listing';
+// current making UL with 3* li
+// need to make a variable for each element to make a ul with 3* li a variable for each ***template literals***
+var ul = '<ul></ul>';
+var li = '<li></li>';
+var about = 'About';
+var home = 'Home';
+var prices = 'Prices'
 document.querySelector('.title>h1').innerHTML = titleHeader;
+document.querySelector('.list').innerHTML = '<ul><li>Home</li><li>about</li><li>Prices</li></ul>';
